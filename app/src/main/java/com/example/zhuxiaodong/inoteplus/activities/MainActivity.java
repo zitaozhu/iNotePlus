7fd12e0d7c66c3edf4bf581cce04a94a39780315
zitaozhu <zhu.zitaosvd@gmail.com>
2018-08-08 12:42:27 +0800

    Changed immerse setting, hide the navigation bar unless its being slided and called.

--- app/src/main/java/com/example/zhuxiaodong/inoteplus/activities/MainActivity.java
+++ app/src/main/java/com/example/zhuxiaodong/inoteplus/activities/MainActivity.java
@@ -65,6 +65,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
         FullScreencall();
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
