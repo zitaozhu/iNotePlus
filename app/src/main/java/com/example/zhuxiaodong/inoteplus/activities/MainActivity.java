ff17577f16fd60d4a4b985f0e2fb8b871e22339e
zitaozhu <zhu.zitaosvd@gmail.com>
2018-08-08 12:41:46 +0800

    Changed immerse setting, hide the navigation bar unless its being slided and called.

--- app/src/main/java/com/example/zhuxiaodong/inoteplus/activities/MainActivity.java
+++ app/src/main/java/com/example/zhuxiaodong/inoteplus/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.zhuxiaodong.inoteplus.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.Menu;
@@ -42,6 +43,26 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(this);
+
+        FullScreencall();
+    }
+
+    public void FullScreencall() {
+        if(Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if(Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        FullScreencall();
     }
 
     @Override
